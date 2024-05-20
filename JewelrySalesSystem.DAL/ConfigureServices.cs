25fadb407ebd302309c947f4d775b63526449470
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-20 17:05:00 +0700

    Add DI Services

---
+++ JewelrySalesSystem.DAL/ConfigureServices.cs
@@ -0,0 +1,21 @@
+﻿using JewelrySalesSystem.DAL.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JewelrySalesSystem.DAL
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection ConfigureDALServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<JewelryDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
