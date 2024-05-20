373a5593320be4e9d78b16d5af5270f7efa3b4c5
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-20 12:13:06 +0700

    Add Entities

---
+++ JewelrySalesSystem.DAL/Entities/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JewelrySalesSystem.DAL.Entities
+{
+    [Table("Role")]
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+        public string? RoleName { get; set; } = string.Empty;
+        public virtual IEnumerable<User>? Users { get; set; }
+    }
+}
---
+++ JewelrySalesSystem.DAL/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JewelrySalesSystem.DAL.Entities
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string? FullName { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; } = string.Empty;
+        public string? Email { get; set; } = string.Empty;
+        public string? Address { get; set; } = string.Empty;
+        public bool Status { get; set; }
+
+        // Role
+        public int RoleId { get; set; }
+        public virtual Role? Role { get; set; }
+    }
+}
