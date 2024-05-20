62d1c34d2f1d8d04c4a8ade7c3452121793440a5
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-20 17:04:25 +0700

    Refactor code

--- JewelrySalesSystem.API/Program.cs
+++ JewelrySalesSystem.API/Program.cs
@@ -1,11 +1,22 @@
+//using JewelrySalesSystem.DAL;
+//using JewelrySalesSystem.DAL.Persistence;
+//using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//builder.Services.AddDbContext<JewelryDbContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//builder.Services.ConfigureDALServices(builder.Configuration);
 
 var app = builder.Build();
 
