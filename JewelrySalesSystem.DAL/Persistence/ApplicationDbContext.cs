05d90dbcf4ecd5ddc6a8bd9c01d9c792a176f7ab
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-20 12:13:42 +0700

    Add DbContext

---
+++ JewelrySalesSystem.DAL/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using JewelrySalesSystem.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JewelrySalesSystem.DAL.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
