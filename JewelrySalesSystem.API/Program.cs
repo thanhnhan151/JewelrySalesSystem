8777c338cb1a2714adf0020bd0f6df7d082ee6e2
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-23 23:26:50 +0700

    Add Dependency Injection

--- JewelrySalesSystem.API/Program.cs
+++ JewelrySalesSystem.API/Program.cs
@@ -1,22 +1,20 @@
-//using JewelrySalesSystem.DAL;
-//using JewelrySalesSystem.DAL.Persistence;
-//using Microsoft.EntityFrameworkCore;
+using JewelrySalesSystem.DAL;
+using JewelrySalesSystem.BAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//builder.Services.AddDbContext<JewelryDbContext>(options =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-//});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.ConfigureDALServices(builder.Configuration);
+builder.Services.ConfigureDALServices(/*builder.Configuration*/);
+
+builder.Services.ConfigureBALServices(/*builder.Configuration*/);
+
 
 var app = builder.Build();
 
