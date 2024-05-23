df12f789b787356a16e7b70c890d82f6766823be
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-23 23:27:04 +0700

    Refactor Unit Of Work

--- JewelrySalesSystem.DAL/Infrastructures/IUnitOfWork.cs
+++ JewelrySalesSystem.DAL/Infrastructures/IUnitOfWork.cs
@@ -5,6 +5,19 @@ namespace JewelrySalesSystem.DAL.Infrastructures
     public interface IUnitOfWork
     {
         IUserRepository Users { get; }
+
+        IWarrantyRepository Warranties { get; }
+
+        IProductRepository Products { get; }
+
+        IMaterialRepository Materials { get; }
+
+        IInvoiceRepository Invoices { get; }
+
+        IGemRepository Gems { get; }
+
+        ICategoryRepository Categories { get; }
+
         Task CompleteAsync();
     }
 }
--- JewelrySalesSystem.DAL/Infrastructures/UnitOfWork.cs
+++ JewelrySalesSystem.DAL/Infrastructures/UnitOfWork.cs
@@ -1,6 +1,53 @@
-﻿namespace JewelrySalesSystem.DAL.Infrastructures
+﻿using JewelrySalesSystem.DAL.Interfaces;
+using JewelrySalesSystem.DAL.Persistence;
+using JewelrySalesSystem.DAL.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace JewelrySalesSystem.DAL.Infrastructures
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly JewelryDbContext _context;
+
+        private readonly ILogger _logger;
+
+        public IUserRepository Users { get; private set; }
+
+        public IWarrantyRepository Warranties { get; private set; }
+
+        public IProductRepository Products { get; private set; }
+
+        public IMaterialRepository Materials { get; private set; }
+
+        public IInvoiceRepository Invoices { get; private set; }
+
+        public IGemRepository Gems { get; private set; }
+
+        public ICategoryRepository Categories { get; private set; }
+
+        public UnitOfWork(
+            JewelryDbContext context,
+            ILoggerFactory loggerFactory)
+        {
+            _context = context;
+
+            _logger = loggerFactory.CreateLogger("logs");
+
+            Users = new UserRepository(_context, _logger);
+
+            Warranties = new WarrantyRepository(_context, _logger);
+
+            Products = new ProductRepository(_context, _logger);
+
+            Materials = new MaterialRepository(_context, _logger);
+
+            Invoices = new InvoiceRepository(_context, _logger);
+
+            Gems = new GemRepository(_context, _logger);
+
+            Categories = new CategoryRepository(_context, _logger);
+        }
+
+        public async Task CompleteAsync() => await _context.SaveChangesAsync();       
     }
 }
