54b13588650f30ea92dbe129c9a975f221f23277
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-25 23:13:31 +0700

    Refactor Pagination for Material

--- JewelrySalesSystem.DAL/Repositories/MaterialRepository.cs
+++ JewelrySalesSystem.DAL/Repositories/MaterialRepository.cs
@@ -4,13 +4,14 @@ using JewelrySalesSystem.DAL.Infrastructures;
 using JewelrySalesSystem.DAL.Interfaces;
 using JewelrySalesSystem.DAL.Persistence;
 using Microsoft.Extensions.Logging;
+using System.Linq.Expressions;
 
 namespace JewelrySalesSystem.DAL.Repositories
 {
     public class MaterialRepository : GenericRepository<Material>, IMaterialRepository
     {
         public MaterialRepository(
-            JewelryDbContext context, 
+            JewelryDbContext context,
             ILogger logger) : base(context, logger)
         {
         }
@@ -24,26 +25,32 @@ namespace JewelrySalesSystem.DAL.Repositories
         {
             IQueryable<Material> materialsQuery = _dbSet;
 
-            //if (!string.IsNullOrWhiteSpace(searchTerm))
-            //{
-            //    materialsQuery = materialsQuery.Where(c =>
-            //        c.FullName.Contains(searchTerm) ||
-            //        c.PhoneNumber.Contains(searchTerm) ||
-            //        c.Email.Contains(searchTerm));
-            //}
+            if (!string.IsNullOrWhiteSpace(searchTerm))
+            {
+                materialsQuery = materialsQuery.Where(c =>
+                    c.MaterialName.Contains(searchTerm));
+            }
 
-            //if (sortOrder?.ToLower() == "desc")
-            //{
-            //    materialsQuery = materialsQuery.OrderByDescending(GetSortProperty(sortColumn));
-            //}
-            //else
-            //{
-            //    materialsQuery = materialsQuery.OrderBy(GetSortProperty(sortColumn));
-            //}
+            if (sortOrder?.ToLower() == "desc")
+            {
+                materialsQuery = materialsQuery.OrderByDescending(GetSortProperty(sortColumn));
+            }
+            else
+            {
+                materialsQuery = materialsQuery.OrderBy(GetSortProperty(sortColumn));
+            }
 
             var materials = await PaginatedList<Material>.CreateAsync(materialsQuery, page, pageSize);
 
             return materials;
         }
+
+        private static Expression<Func<Material, object>> GetSortProperty(string? sortColumn)
+        => sortColumn?.ToLower() switch
+        {
+            "name" => material => material.MaterialName,
+            //"dob" => material => material.DoB,
+            _ => material => material.MaterialId
+        };
     }
 }
