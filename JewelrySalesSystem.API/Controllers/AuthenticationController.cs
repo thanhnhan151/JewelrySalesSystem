e80b40a8e3313c14de6043e20cf9920e959f776b
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-26 23:24:55 +0700

    Refactor code for AuthenticationController

--- JewelrySalesSystem.API/Controllers/AuthenticationController.cs
+++ JewelrySalesSystem.API/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JewelrySalesSystem.BAL.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JewelrySalesSystem.API.Controllers
@@ -7,5 +8,36 @@ namespace JewelrySalesSystem.API.Controllers
     [ApiController]
     public class AuthenticationController : ControllerBase
     {
+        private readonly ILogger<AuthenticationController> _logger;
+        private readonly IUserService _userService;
+
+        public AuthenticationController(
+            ILogger<AuthenticationController> logger,
+            IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync(string email, string password)
+        {
+            try
+            {
+                var result = await _userService.LoginAsync(email, password);
+
+                if (result)
+                    return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return BadRequest(new
+            {
+                ErrorMessage = "Wrong UserName or Password"
+            });
+        }
     }
 }
