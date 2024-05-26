e74477be5871e730b4c9dbddca6893b950f85a8d
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-26 23:24:14 +0700

    Add Unit Of Work to UserService

--- JewelrySalesSystem.BAL/Services/UserService.cs
+++ JewelrySalesSystem.BAL/Services/UserService.cs
@@ -1,8 +1,20 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.DAL.Infrastructures;
 
 namespace JewelrySalesSystem.BAL.Services
 {
     public class UserService : IUserService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> LoginAsync(string email, string passWord)
+        {
+            return await _unitOfWork.Users.LoginAsync(email, passWord);
+        }
     }
 }
