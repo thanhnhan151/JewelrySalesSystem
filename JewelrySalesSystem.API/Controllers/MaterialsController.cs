86403f2dd16da963d6501ade2692d3d4231e0f9f
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-27 23:52:15 +0700

    Add route for MaterialsController

--- JewelrySalesSystem.API/Controllers/MaterialsController.cs
+++ JewelrySalesSystem.API/Controllers/MaterialsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JewelrySalesSystem.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/materials")]
     [ApiController]
     public class MaterialsController : ControllerBase
     {
