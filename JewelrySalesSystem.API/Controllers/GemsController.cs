26c183e274d00d6d03a32e5d62add4bf213d5421
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-27 23:51:55 +0700

    Add route for GemsController

--- JewelrySalesSystem.API/Controllers/GemsController.cs
+++ JewelrySalesSystem.API/Controllers/GemsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JewelrySalesSystem.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/gems")]
     [ApiController]
     public class GemsController : ControllerBase
     {
