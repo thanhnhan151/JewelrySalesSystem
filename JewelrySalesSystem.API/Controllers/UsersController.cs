48c2624903306277ca3c55b12e52347c589dd710
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-27 23:53:06 +0700

    Add route for UsersController

--- JewelrySalesSystem.API/Controllers/UsersController.cs
+++ JewelrySalesSystem.API/Controllers/UsersController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JewelrySalesSystem.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/users")]
     [ApiController]
     public class UsersController : ControllerBase
     {
