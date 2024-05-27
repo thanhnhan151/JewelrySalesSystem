b5ec96bdd585ccdfc2684db9a50e89cfcc407d45
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-27 23:50:09 +0700

    Refactor code

--- JewelrySalesSystem.API/Program.cs
+++ JewelrySalesSystem.API/Program.cs
@@ -1,20 +1,32 @@
 using JewelrySalesSystem.DAL;
 using JewelrySalesSystem.BAL;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.ConfigureDALServices(/*builder.Configuration*/);
+
+builder.Services.ConfigureBALServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 
-builder.Services.ConfigureDALServices(/*builder.Configuration*/);
-
-builder.Services.ConfigureBALServices(/*builder.Configuration*/);
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Jewelry Sales System API"
+    });
+});
 
 builder.Services.AddCors(options =>
 {
