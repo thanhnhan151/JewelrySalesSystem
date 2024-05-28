92a6e988017db20e855ed4a9e4aa17e33040cb05
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-28 23:11:13 +0700

    Refactor InvoiceService

--- JewelrySalesSystem.BAL/Services/InvoiceService.cs
+++ JewelrySalesSystem.BAL/Services/InvoiceService.cs
@@ -1,8 +1,25 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.DAL.Common;
+using JewelrySalesSystem.DAL.Entities;
+using JewelrySalesSystem.DAL.Infrastructures;
 
 namespace JewelrySalesSystem.BAL.Services
 {
     public class InvoiceService : IInvoiceService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public InvoiceService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PaginatedList<Invoice>> PaginationAsync(
+            string? searchTerm,
+            string? sortColumn,
+            string? sortOrder,
+            int page,
+            int pageSize)
+        => await _unitOfWork.Invoices.PaginationAsync(searchTerm, sortColumn, sortOrder, page, pageSize);
     }
 }
