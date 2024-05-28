32a7508ebbc7d76e202d2777b7aa2bf0a2c29a2e
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-28 23:24:38 +0700

    Add GetAllAsync function for CategoriesController

--- JewelrySalesSystem.API/Controllers/CategoriesController.cs
+++ JewelrySalesSystem.API/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.BAL.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +18,31 @@ namespace JewelrySalesSystem.API.Controllers
         {
             _categoryService = categoryService;
             _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(
+            string? searchTerm,
+            string? sortColumn,
+            string? sortOrder,
+            int page,
+            int pageSize)
+        {
+            try
+            {
+                var result = await _categoryService.PaginationAsync(searchTerm, sortColumn, sortOrder, page, pageSize);
+
+                if (result is not null)
+                {
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return BadRequest();
         }
     }
 }
