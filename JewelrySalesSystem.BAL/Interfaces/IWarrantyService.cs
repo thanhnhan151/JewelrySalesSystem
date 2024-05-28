bf66eb16811f91397d8a00922e214388ca23120d
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-28 23:12:08 +0700

    Refactor Warranty Service

--- JewelrySalesSystem.BAL/Interfaces/IWarrantyService.cs
+++ JewelrySalesSystem.BAL/Interfaces/IWarrantyService.cs
@@ -1,6 +1,15 @@
-﻿namespace JewelrySalesSystem.BAL.Interfaces
+﻿using JewelrySalesSystem.DAL.Common;
+using JewelrySalesSystem.DAL.Entities;
+
+namespace JewelrySalesSystem.BAL.Interfaces
 {
     public interface IWarrantyService
     {
+        Task<PaginatedList<Warranty>> PaginationAsync
+            (string? searchTerm
+            , string? sortColumn
+            , string? sortOrder
+            , int page
+            , int pageSize);
     }
 }
--- JewelrySalesSystem.BAL/Services/WarrantyService.cs
+++ JewelrySalesSystem.BAL/Services/WarrantyService.cs
@@ -1,8 +1,25 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.DAL.Common;
+using JewelrySalesSystem.DAL.Entities;
+using JewelrySalesSystem.DAL.Infrastructures;
 
 namespace JewelrySalesSystem.BAL.Services
 {
     public class WarrantyService : IWarrantyService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public WarrantyService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PaginatedList<Warranty>> PaginationAsync(
+            string? searchTerm,
+            string? sortColumn,
+            string? sortOrder,
+            int page,
+            int pageSize)
+        => await _unitOfWork.Warranties.PaginationAsync(searchTerm, sortColumn, sortOrder, page, pageSize);
     }
 }
