9387b7ad1df113b584a80a064969771e06a5c23b
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-28 23:28:12 +0700

    Add GetAllAsync function for ProductsController

--- JewelrySalesSystem.API/Controllers/ProductsController.cs
+++ JewelrySalesSystem.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.BAL.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +18,31 @@ namespace JewelrySalesSystem.API.Controllers
         {
             _logger = logger;
             _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync(
+            string? searchTerm,
+            string? sortColumn,
+            string? sortOrder,
+            int page,
+            int pageSize)
+        {
+            try
+            {
+                var result = await _productService.PaginationAsync(searchTerm, sortColumn, sortOrder, page, pageSize);
+
+                if (result is not null)
+                {
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return BadRequest();
         }
     }
 }
