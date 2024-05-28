80e3beb5181bfca1fa98a917f3ef5567a4ecfd73
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-28 23:10:52 +0700

    Refactor CategoryService

--- JewelrySalesSystem.BAL/Services/CategoryService.cs
+++ JewelrySalesSystem.BAL/Services/CategoryService.cs
@@ -1,8 +1,25 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.DAL.Common;
+using JewelrySalesSystem.DAL.Entities;
+using JewelrySalesSystem.DAL.Infrastructures;
 
 namespace JewelrySalesSystem.BAL.Services
 {
     public class CategoryService : ICategoryService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PaginatedList<Category>> PaginationAsync(
+            string? searchTerm,
+            string? sortColumn,
+            string? sortOrder,
+            int page,
+            int pageSize)
+        => await _unitOfWork.Categories.PaginationAsync(searchTerm, sortColumn, sortOrder, page, pageSize);
     }
 }
