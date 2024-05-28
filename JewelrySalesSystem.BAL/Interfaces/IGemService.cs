a3e2fa6b802795a5631b5a42bd63375057130c9a
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-28 23:11:03 +0700

    Refactor GemService

--- JewelrySalesSystem.BAL/Interfaces/IGemService.cs
+++ JewelrySalesSystem.BAL/Interfaces/IGemService.cs
@@ -1,6 +1,15 @@
-﻿namespace JewelrySalesSystem.BAL.Interfaces
+﻿using JewelrySalesSystem.DAL.Common;
+using JewelrySalesSystem.DAL.Entities;
+
+namespace JewelrySalesSystem.BAL.Interfaces
 {
     public interface IGemService
     {
+        Task<PaginatedList<Gem>> PaginationAsync
+            (string? searchTerm
+            , string? sortColumn
+            , string? sortOrder
+            , int page
+            , int pageSize);
     }
 }
--- JewelrySalesSystem.BAL/Services/GemService.cs
+++ JewelrySalesSystem.BAL/Services/GemService.cs
@@ -1,8 +1,25 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.DAL.Common;
+using JewelrySalesSystem.DAL.Entities;
+using JewelrySalesSystem.DAL.Infrastructures;
 
 namespace JewelrySalesSystem.BAL.Services
 {
     public class GemService : IGemService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public GemService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PaginatedList<Gem>> PaginationAsync(
+            string? searchTerm,
+            string? sortColumn,
+            string? sortOrder,
+            int page,
+            int pageSize)
+        => await _unitOfWork.Gems.PaginationAsync(searchTerm, sortColumn, sortOrder, page, pageSize);
     }
 }
