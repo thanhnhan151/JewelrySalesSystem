b08796d2d2323ccf3516d9fdfcc1ea75e901d098
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-28 23:11:25 +0700

    Refactor MaterialService

--- JewelrySalesSystem.BAL/Services/MaterialService.cs
+++ JewelrySalesSystem.BAL/Services/MaterialService.cs
@@ -1,8 +1,25 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.DAL.Common;
+using JewelrySalesSystem.DAL.Entities;
+using JewelrySalesSystem.DAL.Infrastructures;
 
 namespace JewelrySalesSystem.BAL.Services
 {
     public class MaterialService : IMaterialService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MaterialService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<PaginatedList<Material>> PaginationAsync(
+            string? searchTerm,
+            string? sortColumn,
+            string? sortOrder,
+            int page,
+            int pageSize)
+        => await _unitOfWork.Materials.PaginationAsync(searchTerm, sortColumn, sortOrder, page, pageSize);
     }
 }
