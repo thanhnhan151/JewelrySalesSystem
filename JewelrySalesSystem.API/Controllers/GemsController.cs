178ecbde1d17ea457a2a3308b6d89b76688c7c9b
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-05-31 23:38:43 +0700

    Add, Update and GetById for GemsController

--- JewelrySalesSystem.API/Controllers/GemsController.cs
+++ JewelrySalesSystem.API/Controllers/GemsController.cs
@@ -1,5 +1,5 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
-using JewelrySalesSystem.BAL.Services;
+using JewelrySalesSystem.DAL.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -46,21 +46,53 @@ namespace JewelrySalesSystem.API.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateAsync()
+        public async Task<IActionResult> AddAsync(Gem gem)
         {
-            return BadRequest();
+            try
+            {
+                await _gemService.AddAsync(gem);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetByIdAsync(int id)
         {
+            try
+            {
+                var result = await _gemService.GetByIdAsync(id);
+
+                if (result is not null)
+                {
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
             return BadRequest();
         }
 
         [HttpPut]
-        public async Task<IActionResult> UpdateAsync()
+        public async Task<IActionResult> UpdateAsync(Gem gem)
         {
-            return BadRequest();
+            try
+            {
+                await _gemService.UpdateAsync(gem);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
