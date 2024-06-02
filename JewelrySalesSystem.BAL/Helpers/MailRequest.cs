12b897ca3cd1d38c6d6c1d30451ec886d726c7d2
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-02 17:32:47 +0700

    Add new Helpers directory

---
+++ JewelrySalesSystem.BAL/Helpers/MailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace JewelrySalesSystem.BAL.Helpers
+{
+    public class MailRequest
+    {
+        public string ToEmail { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
