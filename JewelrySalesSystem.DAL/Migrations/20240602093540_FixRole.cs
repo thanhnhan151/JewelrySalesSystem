454bb017b335ae5ed9e9e1755b31367364ec3b7a
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-02 17:31:42 +0700

    Add New Migrations

---
+++ JewelrySalesSystem.DAL/Migrations/20240602093540_FixRole.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JewelrySalesSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleName",
+                table: "Role",
+                type: "nvarchar(15)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleName",
+                table: "Role",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
