131d4b4c6e5d7beb3d57ab87d99f60d517cd4637
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-02 17:30:56 +0700

    Add Generate Access Token Function

--- JewelrySalesSystem.API/Controllers/AuthenticationController.cs
+++ JewelrySalesSystem.API/Controllers/AuthenticationController.cs
@@ -1,6 +1,11 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.DAL.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace JewelrySalesSystem.API.Controllers
 {
@@ -10,13 +15,16 @@ namespace JewelrySalesSystem.API.Controllers
     {
         private readonly ILogger<AuthenticationController> _logger;
         private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
 
         public AuthenticationController(
             ILogger<AuthenticationController> logger,
-            IUserService userService)
+            IUserService userService,
+            IConfiguration configuration)
         {
             _logger = logger;
             _userService = userService;
+            _configuration = configuration;
         }
 
         [HttpPost]
@@ -26,8 +34,11 @@ namespace JewelrySalesSystem.API.Controllers
             {
                 var result = await _userService.LoginAsync(email, password);
 
-                if (result)
-                    return Ok(result);
+                if (result != null)
+                {
+                    var accessToken = GenerateAccessToken(result);
+                    return Ok(accessToken);
+                }
             }
             catch (Exception ex)
             {
@@ -39,5 +50,32 @@ namespace JewelrySalesSystem.API.Controllers
                 ErrorMessage = "Wrong UserName or Password"
             });
         }
+
+        #region Generate Access Token
+        private string GenerateAccessToken(User user)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.RoleName),
+                new Claim("Id", user.UserId.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Key").Value!));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                        claims: claims,
+                        expires: DateTime.UtcNow.AddHours(1),
+                        signingCredentials: credentials
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+        #endregion
     }
 }
