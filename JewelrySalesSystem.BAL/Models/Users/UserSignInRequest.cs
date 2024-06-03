22452e70945d7c1c0bc98692b827752da9026ecf
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-03 09:46:41 +0700

    Add UseSignInRequest class

---
+++ JewelrySalesSystem.BAL/Models/Users/UserSignInRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace JewelrySalesSystem.BAL.Models.Users
+{
+    public class UserSignInRequest
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
