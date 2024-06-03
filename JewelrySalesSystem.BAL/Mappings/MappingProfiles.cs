8eb7342c87989ee8a8844521846133f1f7edb94d
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-03 12:01:00 +0700

    Add AutoMapper

--- JewelrySalesSystem.BAL/Mappings/MappingProfiles.cs
+++ JewelrySalesSystem.BAL/Mappings/MappingProfiles.cs
@@ -1,9 +1,18 @@
 ﻿using AutoMapper;
+using JewelrySalesSystem.BAL.Models.Users;
+using JewelrySalesSystem.DAL.Common;
+using JewelrySalesSystem.DAL.Entities;
 
 namespace JewelrySalesSystem.BAL.Mappings
 {
     public class MappingProfiles : Profile
     {
-        public MappingProfiles() { }
+        public MappingProfiles()
+        {
+            CreateMap<User, GetUserRequest>();
+            CreateMap<PaginatedList<User>, PaginatedList<GetUserRequest>>();
+            CreateMap<CreateUserRequest, User>();
+            CreateMap<UpdateUserRequest, User>();
+        }
     }
 }
