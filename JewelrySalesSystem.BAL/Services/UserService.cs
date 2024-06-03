17ff3253e65e8e67c5eb752dfd1c6d7926b903bb
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-03 12:00:15 +0700

    Fix return type and parameter

--- JewelrySalesSystem.BAL/Services/UserService.cs
+++ JewelrySalesSystem.BAL/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using JewelrySalesSystem.BAL.Interfaces;
+﻿using AutoMapper;
+using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.BAL.Models.Users;
 using JewelrySalesSystem.DAL.Common;
 using JewelrySalesSystem.DAL.Entities;
 using JewelrySalesSystem.DAL.Infrastructures;
@@ -8,38 +10,43 @@ namespace JewelrySalesSystem.BAL.Services
     public class UserService : IUserService
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public UserService(IUnitOfWork unitOfWork)
+        public UserService(
+            IUnitOfWork unitOfWork
+            , IMapper mapper)
         {
+            _mapper = mapper;
             _unitOfWork = unitOfWork;
         }
 
         public async Task<User?> LoginAsync(string userName, string passWord)
             => await _unitOfWork.Users.LoginAsync(userName, passWord);
 
-        public async Task<PaginatedList<User>> PaginationAsync(
+        public async Task<PaginatedList<GetUserRequest>> PaginationAsync(
             string? searchTerm,
             string? sortColumn,
             string? sortOrder,
             int page,
             int pageSize)
-        => await _unitOfWork.Users.PaginationAsync(searchTerm, sortColumn, sortOrder, page, pageSize);
+        => _mapper.Map<PaginatedList<GetUserRequest>>(await _unitOfWork.Users.PaginationAsync(searchTerm, sortColumn, sortOrder, page, pageSize));
 
-        public async Task<User> AddAsync(User user)
-        {
-            var result = _unitOfWork.Users.AddEntity(user);
+        public async Task<CreateUserRequest> AddAsync(CreateUserRequest createUserRequest)
+        {  
+
+            var result = _unitOfWork.Users.AddEntity(_mapper.Map<User>(createUserRequest));
 
             await _unitOfWork.CompleteAsync();
 
-            return result;
+            return createUserRequest;
         }
 
-        public async Task UpdateAsync(User user)
+        public async Task UpdateAsync(UpdateUserRequest updateUserRequest)
         {
-            _unitOfWork.Users.UpdateEntity(user);
+            _unitOfWork.Users.UpdateEntity(_mapper.Map<User>(updateUserRequest));
             await _unitOfWork.CompleteAsync();
         }
 
-        public async Task<User?> GetByIdAsync(int id) => await _unitOfWork.Users.GetByIdAsync(id);
+        public async Task<GetUserRequest?> GetByIdAsync(int id) => _mapper.Map<GetUserRequest>(await _unitOfWork.Users.GetByIdAsync(id));
     }
 }
