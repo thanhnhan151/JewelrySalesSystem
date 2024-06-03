993217f587343a5318c610a96c66914161c628dc
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-03 09:45:48 +0700

    Add Migrations

---
+++ JewelrySalesSystem.DAL/Migrations/20240602155544_RefactorEntityField.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JewelrySalesSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorEntityField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstImage",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "SecondImage",
+                table: "Product");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "User",
+                type: "nvarchar(12)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstImage",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondImage",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
