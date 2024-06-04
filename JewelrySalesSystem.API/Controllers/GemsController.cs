1fa2561572aa9cf12ffd453ce56f87c51a255333
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-04 11:37:00 +0700

    Add API Documentation for GemsController

--- JewelrySalesSystem.API/Controllers/GemsController.cs
+++ JewelrySalesSystem.API/Controllers/GemsController.cs
@@ -20,6 +20,22 @@ namespace JewelrySalesSystem.API.Controllers
             _gemService = gemService;
         }
 
+        #region Get All Gems
+        /// <summary>
+        /// Get all gems in the system
+        /// </summary>
+        /// <param name="page">Current page the user is on</param>
+        /// <param name="pageSize">Number of entities you want to show</param>
+        /// <param name="searchTerm">Search query</param>
+        /// <param name="sortColumn">Column you want to sort</param>
+        /// <param name="sortOrder">Sort column by ascending or descening</param>
+        /// <returns>A list of all users</returns>
+        /// <response code="200">Return all gems in the system</response>
+        /// <response code="400">If no gems are in the system</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server</response>
         [HttpGet]
         public async Task<IActionResult> GetAllAsync(
             string? searchTerm,
@@ -44,7 +60,33 @@ namespace JewelrySalesSystem.API.Controllers
 
             return BadRequest();
         }
+        #endregion
 
+        #region Add Gem
+        /// <summary>
+        /// Add a gem in the system
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     {
+        ///       "userName": "testaccount",
+        ///       "fullName": "Nguyen Van B",
+        ///       "phoneNumber": "0999123456",
+        ///       "email": "testemail@gmail.com",
+        ///       "password" : "test",
+        ///       "address" : "test",
+        ///       "roleId" : 3
+        ///     }
+        ///         
+        /// </remarks> 
+        /// <returns>Gem that was created</returns>
+        /// <response code="200">Gem that was created</response>
+        /// <response code="400">Failed validation</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server</response>
         [HttpPost]
         public async Task<IActionResult> AddAsync(Gem gem)
         {
@@ -59,7 +101,20 @@ namespace JewelrySalesSystem.API.Controllers
                 throw new Exception(ex.Message);
             }
         }
+        #endregion
 
+        #region Get Gem By Id
+        /// <summary>
+        /// Get a gem in the system
+        /// </summary>
+        /// <param name="id">Id of the gem you want to get</param>
+        /// <returns>A product</returns>
+        /// <response code="200">Return a gem in the system</response>
+        /// <response code="400">If the gem is null</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server</response>
         [HttpGet("{id}")]
         public async Task<IActionResult> GetByIdAsync(int id)
         {
@@ -79,7 +134,34 @@ namespace JewelrySalesSystem.API.Controllers
 
             return BadRequest();
         }
+        #endregion
 
+        #region Update Gem
+        /// <summary>
+        /// Update a gem in the system
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     {
+        ///       "userId" : 2,
+        ///       "userName": "newtestaccount",
+        ///       "fullName": "Nguyen Van C",
+        ///       "phoneNumber": "0999123456",
+        ///       "email": "testemail@gmail.com",
+        ///       "password" : "test",
+        ///       "address" : "test",
+        ///       "roleId" : 2
+        ///     }
+        ///         
+        /// </remarks> 
+        /// <returns>No content</returns>
+        /// <response code="204">No content</response>
+        /// <response code="400">Gem does not exist</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server</response>
         [HttpPut]
         public async Task<IActionResult> UpdateAsync(Gem gem)
         {
@@ -94,5 +176,6 @@ namespace JewelrySalesSystem.API.Controllers
                 throw new Exception(ex.Message);
             }
         }
+        #endregion
     }
 }
