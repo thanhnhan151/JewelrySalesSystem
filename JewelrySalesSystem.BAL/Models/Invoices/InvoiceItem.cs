cf97fb68cd9fade6ce90ab69cd97ef27bb53b6cb
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-05 13:05:19 +0700

    Add InvoiceViewModel

---
+++ JewelrySalesSystem.BAL/Models/Invoices/InvoiceItem.cs
@@ -0,0 +1,8 @@
+﻿namespace JewelrySalesSystem.BAL.Models.Invoices
+{
+    public class InvoiceItem
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public float ProductPrice { get; set; }
+    }
+}
