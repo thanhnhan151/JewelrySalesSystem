01fb7b343d7f153d6bd8b9efb7255d26566eb76f
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-05 11:37:06 +0700

    Add Mapping for Product, Gem and Material

--- JewelrySalesSystem.BAL/Mappings/MappingProfiles.cs
+++ JewelrySalesSystem.BAL/Mappings/MappingProfiles.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using JewelrySalesSystem.BAL.Models.Gems;
+using JewelrySalesSystem.BAL.Models.Materials;
+using JewelrySalesSystem.BAL.Models.Products;
 using JewelrySalesSystem.BAL.Models.Users;
 using JewelrySalesSystem.DAL.Common;
 using JewelrySalesSystem.DAL.Entities;
@@ -9,10 +12,25 @@ namespace JewelrySalesSystem.BAL.Mappings
     {
         public MappingProfiles()
         {
+            #region User
             CreateMap<User, GetUserResponse>();
             CreateMap<PaginatedList<User>, PaginatedList<GetUserResponse>>();
             CreateMap<CreateUserRequest, User>();
             CreateMap<UpdateUserRequest, User>();
+            #endregion
+
+            #region Product
+            CreateMap<Product, GetProductResponse>();
+            CreateMap<PaginatedList<Product>, PaginatedList<GetProductResponse>>();
+            #endregion
+
+            #region Gem
+            CreateMap<List<Gem>, List<GemItem>>();
+            #endregion
+
+            #region Material
+            CreateMap<List<Material>, List<MaterialItem>>();
+            #endregion
         }
     }
 }
