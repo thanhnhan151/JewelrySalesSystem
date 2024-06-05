5a9915b1b20d819413d850cd08b06def01683a6b
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-05 11:38:12 +0700

    Refactor code

--- JewelrySalesSystem.API/Controllers/ProductsController.cs
+++ JewelrySalesSystem.API/Controllers/ProductsController.cs
@@ -120,7 +120,7 @@ namespace JewelrySalesSystem.API.Controllers
         {
             try
             {
-                var result = await _productService.GetByIdAsync(id);
+                var result = await _productService.GetByIdWithIncludeAsync(id);
 
                 if (result is not null)
                 {
