8028e0d487660b3e176d0da8d1d29aa9c18b2c00
Thanh3Bien <thanhlv1793@gmail.com>
2024-06-06 07:20:05 +0700

    Use Auto Mapper in API

--- JewelrySalesSystem.API/Controllers/RolesController.cs
+++ JewelrySalesSystem.API/Controllers/RolesController.cs
@@ -1,4 +1,6 @@
-﻿using JewelrySalesSystem.BAL.Interfaces;
+﻿using AutoMapper;
+using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.BAL.Models.Roles;
 using JewelrySalesSystem.DAL.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,20 +13,30 @@ namespace JewelrySalesSystem.API.Controllers
     {
         private readonly ILogger<RolesController> _logger;
         private readonly IRoleService _roleService;
-
+        private readonly IMapper _mapper;
         public RolesController(
             ILogger<RolesController> logger,
-            IRoleService roleService)
+            IRoleService roleService, IMapper mapper)
         {
             _logger = logger;
             _roleService = roleService;
+            _mapper = mapper;
         }
 
         /*Change here*/
         [HttpPost]
-        public async Task<IActionResult> AddRoleAsync(Role role)
+        public async Task<IActionResult> AddRoleAsync(RoleViewModel roleViewModel)
         {
+            //var newRole = await _roleService.AddRoleAsync(role);
+            //return Ok(newRole);
+            //Map the RoleViewModel to a Role object
+           var role = _mapper.Map<Role>(roleViewModel);
+
+            // Call the service to add the new role
             var newRole = await _roleService.AddRoleAsync(role);
+
+            // Map the new Role back to a RoleViewModel and return it
+            //var newRoleViewModel = _mapper.Map<RoleViewModel>(newRole);
             return Ok(newRole);
         }
 
--- JewelrySalesSystem.API/Program.cs
+++ JewelrySalesSystem.API/Program.cs
@@ -19,6 +19,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+//Change here
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 builder.Services.AddSwaggerGen(options =>
 {
     // using System.Reflection;
