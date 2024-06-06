368dc80486f41c577f8922b981edff3a42819a01
Thanh3Bien <thanhlv1793@gmail.com>
2024-06-06 11:10:23 +0700

    Delete Changing in Program.cs

--- JewelrySalesSystem.API/Program.cs
+++ JewelrySalesSystem.API/Program.cs
@@ -19,9 +19,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-//Change here
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-
 builder.Services.AddSwaggerGen(options =>
 {
     // using System.Reflection;
