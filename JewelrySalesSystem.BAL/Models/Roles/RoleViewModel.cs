95ed402b4a101ec8b543b9d02186c1140802970a
Thanh3Bien <thanhlv1793@gmail.com>
2024-06-06 10:30:47 +0700

    Delete useless folder

--- JewelrySalesSystem.BAL/Models/Roles/RoleViewModel.cs
+++ JewelrySalesSystem.BAL/Models/Roles/RoleViewModel.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using JewelrySalesSystem.BAL.Models.Users;
+
 using JewelrySalesSystem.DAL.Entities;
 
 namespace JewelrySalesSystem.BAL.Models.Roles
 {
     public class RoleViewModel
     {
-        public int RoleId { get; set; }
         public string RoleName { get; set; }
-        public virtual ICollection<UserViewModel> Users { get; set; }
     }
 }
