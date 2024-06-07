d00e6d41cab7658ca2864b0f244294b36cf93640
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-07 16:54:35 +0700

    Add CreateRequest Class

---
+++ JewelrySalesSystem.BAL/Models/Invoices/CreateInvoiceRequest.cs
@@ -0,0 +1,22 @@
+﻿using JewelrySalesSystem.DAL.Entities;
+
+namespace JewelrySalesSystem.BAL.Models.Invoices
+{
+    public class CreateInvoiceRequest
+    {
+        public bool Status { get; set; }
+        public bool InvoiceType { get; set; }
+
+        // Customer
+        public int CustomerId { get; set; }
+
+        // User
+        public int UserId { get; set; }
+
+        // Warranty
+        public int WarrantyId { get; set; }
+
+        // Invoice Details
+        public virtual ICollection<int> InvoiceDetails { get; set; } = new List<int>();
+    }
+}
---
+++ JewelrySalesSystem.BAL/Models/Products/CreateProductRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace JewelrySalesSystem.BAL.Models.Products
+{
+    public class CreateProductRequest
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public int PercentPriceRate { get; set; }
+        public float ProductionCost { get; set; }
+        public string FeaturedImage { get; set; } = string.Empty;
+
+        // Category
+        public int CategoryId { get; set; }
+
+        // Product Type
+        public int ProductTypeId { get; set; }
+
+        // Gender
+        public int GenderId { get; set; }
+
+        // Colour
+        public int ColourId { get; set; }
+
+        // Product Gems
+        public ICollection<int> Gems { get; set; } = new List<int>();
+
+        // Product Materials
+        public ICollection<int> Materials { get; set; } = new List<int>();
+    }
+}
