192fde62bf352f1577e595329cd07daf0bf43c18
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-10 21:40:19 +0700

    Add ProductId property to InvoiceItem

--- JewelrySalesSystem.BAL/Models/Invoices/InvoiceItem.cs
+++ JewelrySalesSystem.BAL/Models/Invoices/InvoiceItem.cs
@@ -2,6 +2,7 @@
 {
     public class InvoiceItem
     {
+        public int ProductId { get; set; }
         public string ProductName { get; set; } = string.Empty;
         public float ProductPrice { get; set; }
     }
