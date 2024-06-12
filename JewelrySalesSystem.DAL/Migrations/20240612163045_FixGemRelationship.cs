32f0316d2db902287e134cd6e6381dac4ba69d4a
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-13 00:47:23 +0700

    Add Migrations

---
+++ JewelrySalesSystem.DAL/Migrations/20240612163045_FixGemRelationship.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JewelrySalesSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixGemRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GemPriceList_GemId",
+                table: "GemPriceList");
+
+            migrationBuilder.DropColumn(
+                name: "EffDate",
+                table: "GemPriceList");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GemPriceList_GemId",
+                table: "GemPriceList",
+                column: "GemId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_GemPriceList_GemId",
+                table: "GemPriceList");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EffDate",
+                table: "GemPriceList",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GemPriceList_GemId",
+                table: "GemPriceList",
+                column: "GemId");
+        }
+    }
+}
