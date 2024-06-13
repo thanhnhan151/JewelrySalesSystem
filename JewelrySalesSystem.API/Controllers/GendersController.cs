302dfaed311608bdd98c78705140c683cc0c08d6
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-13 11:15:20 +0700

    Refactor GendersController

--- JewelrySalesSystem.API/Controllers/GendersController.cs
+++ JewelrySalesSystem.API/Controllers/GendersController.cs
@@ -19,18 +19,39 @@ namespace JewelrySalesSystem.API.Controllers
             _logger = logger;
             _genderService = genderService;
         }
+
+        #region Add Gender
+        /// <summary>
+        /// Add a gender in the system
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     {
+        ///       "genderName": "Test Gender"
+        ///     }
+        ///         
+        /// </remarks> 
+        /// <returns>Gender that was created</returns>
+        /// <response code="200">Gender that was created</response>
+        /// <response code="400">Failed validation</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server</response>
         [HttpPost]
-        public async Task<IActionResult> AddGender(GenderModel gender)
+        public async Task<IActionResult> AddGender(CreateGenderRequest createGenderRequest)
         {
             try
             {
-                await _genderService.AddGender(gender);
-                return Ok();
+                var result = await _genderService.AddGender(createGenderRequest);
+                return Ok(result);
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
         }
+        #endregion
     }
 }
