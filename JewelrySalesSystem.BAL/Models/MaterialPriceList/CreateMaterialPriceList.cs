f48a59f4506dacc139ed4dad29c0669e19aec3fc
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-13 11:16:36 +0700

    Refactor Material Price List

--- JewelrySalesSystem.BAL/Models/MaterialPriceList/CreateMaterialPriceList.cs
+++ JewelrySalesSystem.BAL/Models/MaterialPriceList/CreateMaterialPriceList.cs
@@ -2,9 +2,7 @@
 {
     public class CreateMaterialPriceList
     {
-        public int MaterialId { get; set; } 
         public float BuyPrice { get; set; }
         public float SellPrice { get; set; }
-        public DateTime EffDate { get; set; }
     }
 }
