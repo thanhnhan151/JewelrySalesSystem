008f7eb289eefa0ff3e93e1cdbe8813f16143148
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-13 11:15:38 +0700

    Refactor RolesController

--- JewelrySalesSystem.API/Controllers/RolesController.cs
+++ JewelrySalesSystem.API/Controllers/RolesController.cs
@@ -1,5 +1,7 @@
 ﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.BAL.Models.Categories;
 using JewelrySalesSystem.BAL.Models.Roles;
+using JewelrySalesSystem.BAL.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,20 +23,39 @@ namespace JewelrySalesSystem.API.Controllers
 
         }
 
+        #region Add Role
+        /// <summary>
+        /// Add a role in the system
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     {
+        ///       "roleName": "Test Role"
+        ///     }
+        ///         
+        /// </remarks> 
+        /// <returns>Role that was created</returns>
+        /// <response code="200">Role that was created</response>
+        /// <response code="400">Failed validation</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server</response>
         [HttpPost]
-        public async Task<IActionResult> AddRoleAsync([FromBody] RoleViewModel roleViewModel)
+        public async Task<IActionResult> AddAsync([FromBody] CreateRoleRequest createRoleRequest)
         {
-            //var newRole = await _roleService.AddRoleAsync(role);
-            //return Ok(newRole);
-            //Map the RoleViewModel to a Role object
+            try
+            {
+                var result = await _roleService.AddRoleAsync(createRoleRequest);
 
-            // Call the service to add the new role
-            var newRole = await _roleService.AddRoleAsync(roleViewModel);
-
-            // Map the new Role back to a RoleViewModel and return it
-            //var newRoleViewModel = _mapper.Map<RoleViewModel>(newRole);
-            return Ok(newRole);
+                return Ok(createRoleRequest);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
-
+        #endregion
     }
 }
