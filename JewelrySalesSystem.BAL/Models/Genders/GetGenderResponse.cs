a016d4b31095de000162595cfd04fc4b89942d6a
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-13 23:25:36 +0700

    Add Mapping Models

---
+++ JewelrySalesSystem.BAL/Models/Genders/GetGenderResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace JewelrySalesSystem.BAL.Models.Genders
+{
+    public class GetGenderResponse
+    {
+        public int GenderId { get; set; }
+        public string GenderName { get; set; } = string.Empty;
+    }
+}
---
+++ JewelrySalesSystem.BAL/Models/ProductTypes/GetProductTypeResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace JewelrySalesSystem.BAL.Models.ProductTypes
+{
+    public class GetProductTypeResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
