f049f86aeecc2b46768e10d543257136f9e25c02
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-13 23:26:01 +0700

    Refactor BAL Interfaces

--- JewelrySalesSystem.BAL/Interfaces/IGenderService.cs
+++ JewelrySalesSystem.BAL/Interfaces/IGenderService.cs
@@ -5,5 +5,9 @@ namespace JewelrySalesSystem.BAL.Interfaces
     public interface IGenderService
     {
         Task<CreateGenderRequest> AddGender(CreateGenderRequest gender);
+
+        Task<GetGenderResponse?> GetByIdAsync(int id);
+
+        Task<List<GetGenderResponse>> GetAllAsync();
     }
 }
--- JewelrySalesSystem.BAL/Interfaces/IProductTypeService.cs
+++ JewelrySalesSystem.BAL/Interfaces/IProductTypeService.cs
@@ -1,6 +1,11 @@
-﻿namespace JewelrySalesSystem.BAL.Interfaces
+﻿using JewelrySalesSystem.BAL.Models.ProductTypes;
+
+namespace JewelrySalesSystem.BAL.Interfaces
 {
     public interface IProductTypeService
     {
+        Task<GetProductTypeResponse?> GetByIdAsync(int id);
+
+        Task<List<GetProductTypeResponse>> GetAllAsync();
     }
 }
