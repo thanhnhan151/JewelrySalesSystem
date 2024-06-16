a0a12d71d66d90110a5046254263587285d5438f
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-16 23:48:51 +0700

    Add BAL Interface

---
+++ JewelrySalesSystem.BAL/Interfaces/ICustomerService.cs
@@ -0,0 +1,9 @@
+﻿using JewelrySalesSystem.BAL.Models.Customers;
+
+namespace JewelrySalesSystem.BAL.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<GetCustomerPointResponse?> GetCustomerPointByNameAsync(string customerName);
+    }
+}
