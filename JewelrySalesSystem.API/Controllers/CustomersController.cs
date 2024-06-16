7c80f9e47f03e358e718e06b9ecdb4e21c3dbb71
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-16 23:48:37 +0700

    Add CustomersController

---
+++ JewelrySalesSystem.API/Controllers/CustomersController.cs
@@ -0,0 +1,58 @@
+﻿using JewelrySalesSystem.BAL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JewelrySalesSystem.API.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+        private readonly ILogger<CustomersController> _logger;
+
+        public CustomersController(
+            ILogger<CustomersController> logger,
+            ICustomerService customerService)
+        {
+            _customerService = customerService;
+            _logger = logger;
+        }
+
+        #region Get Customer Point By Name
+        /// <summary>
+        /// Get user point based on name in the system
+        /// </summary>
+        /// <param name="customerName">Name of the category you want to get</param>
+        /// <returns>Customer point</returns>
+        /// <response code="200">Return Customer point</response>
+        /// <response code="400">If the customer is null</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server</response>
+        [HttpGet("{customerName}/point")]
+        public async Task<IActionResult> GetCustomerPointByNameAsync(string customerName)
+        {
+            try
+            {
+                var result = await _customerService.GetCustomerPointByNameAsync(customerName);
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return NotFound(new
+            {
+                ErrorMessage = $"Customer with {customerName} does not exist"
+            });
+        }
+        #endregion
+    }
+}
