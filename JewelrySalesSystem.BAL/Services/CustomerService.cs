d91d15084a7742ee32e38a6e6b085e50cc39530d
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-16 23:49:25 +0700

    Add BAL Service

---
+++ JewelrySalesSystem.BAL/Services/CustomerService.cs
@@ -0,0 +1,32 @@
+﻿using JewelrySalesSystem.BAL.Interfaces;
+using JewelrySalesSystem.BAL.Models.Customers;
+using JewelrySalesSystem.DAL.Infrastructures;
+
+namespace JewelrySalesSystem.BAL.Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CustomerService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<GetCustomerPointResponse?> GetCustomerPointByNameAsync(string customerName)
+        {
+            var customer = await _unitOfWork.Customers.GetCustomerPointByNameAsync(customerName);
+
+            if (customer != null)
+            {
+                var result = new GetCustomerPointResponse
+                {
+                    Point = customer.Point
+                };
+
+                return result;
+            }
+            return null;
+        }
+    }
+}
