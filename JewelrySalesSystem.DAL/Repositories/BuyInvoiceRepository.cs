e4dbf86dca5d636c32cc3517cc57b9e51442a487
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-17 18:08:11 +0700

    Add DAL Repository

---
+++ JewelrySalesSystem.DAL/Repositories/BuyInvoiceRepository.cs
@@ -0,0 +1,20 @@
+﻿using JewelrySalesSystem.DAL.Entities;
+using JewelrySalesSystem.DAL.Infrastructures;
+using JewelrySalesSystem.DAL.Interfaces;
+using JewelrySalesSystem.DAL.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace JewelrySalesSystem.DAL.Repositories
+{
+    public class BuyInvoiceRepository : GenericRepository<BuyInvoice>, IBuyInvoiceRepository
+    {
+        public BuyInvoiceRepository(JewelryDbContext context, ILogger logger) : base(context, logger)
+        {
+        }
+
+        public async Task<List<BuyInvoice>> GetAllAsync() => await _dbSet.Include(o => o.Items).ToListAsync();
+
+        public async Task<BuyInvoice?> GetByIdWithInclude(int id) => await _dbSet.Include(o => o.Items).FirstOrDefaultAsync(o => o.BuyInvoiceId == id);
+    }
+}
