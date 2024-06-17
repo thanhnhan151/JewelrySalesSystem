21fc4c1bc5cd7459a102cafbe23469f8e9f3578c
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-17 18:08:02 +0700

    Add DAL Interface

---
+++ JewelrySalesSystem.DAL/Interfaces/IBuyInvoiceRepository.cs
@@ -0,0 +1,11 @@
+﻿using JewelrySalesSystem.DAL.Entities;
+using JewelrySalesSystem.DAL.Infrastructures;
+
+namespace JewelrySalesSystem.DAL.Interfaces
+{
+    public interface IBuyInvoiceRepository : IGenericRepository<BuyInvoice>
+    {
+        Task<List<BuyInvoice>> GetAllAsync();
+        Task<BuyInvoice?> GetByIdWithInclude(int id);
+    }
+}
