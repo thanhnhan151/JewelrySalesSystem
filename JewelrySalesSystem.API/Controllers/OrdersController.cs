ac4016fae6dc0d6fac2e1cb00bc8d39d872b6c56
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-17 11:54:09 +0700

    Refactor code

--- JewelrySalesSystem.API/Controllers/OrdersController.cs
+++ JewelrySalesSystem.API/Controllers/OrdersController.cs
@@ -18,5 +18,79 @@ namespace JewelrySalesSystem.API.Controllers
             _logger = logger;
             _orderService = orderService;
         }
+
+        #region Get ALl Orders
+        /// <summary>
+        /// Get all orders in the system
+        /// </summary>
+        /// <returns>A list of all orders</returns>
+        /// <response code="200">Return all orders in the system</response>
+        /// <response code="400">If no orders are in the system</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server</response>
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            try
+            {
+                var result = await _orderService.GetAllAsync();
+
+                if (result is not null)
+                {
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return NotFound();
+        }
+        #endregion
+
+        #region Get Order By Id
+        /// <summary>
+        /// Get a order based on Id in the system
+        /// </summary>
+        /// <param name="id">Id of the order you want to get</param>
+        /// <returns>A order</returns>
+        /// <response code="200">Return a order in the system</response>
+        /// <response code="400">If the order is null</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="403">Forbidden</response>
+        /// <response code="404">Not Found</response>
+        /// <response code="500">Internal Server</response>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            try
+            {
+                var result = await _orderService.GetByIdWithIncludeAsync(id);
+
+                if (result is not null)
+                {
+                    return Ok(result);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return NotFound(new
+            {
+                ErrorMessage = $"Order with {id} does not exist"
+            });
+        }
+        #endregion
+
+        #region Add Order
+        #endregion
+
+        #region Update Order
+        #endregion
     }
 }
