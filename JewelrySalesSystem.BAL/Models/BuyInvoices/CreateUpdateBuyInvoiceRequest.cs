0b3f228d049981535d1922dd782f67d7abdd8fc2
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-18 17:23:39 +0700

    Refactor Create Buy Invoice Model

--- JewelrySalesSystem.BAL/Models/BuyInvoices/CreateUpdateBuyInvoiceRequest.cs
+++ JewelrySalesSystem.BAL/Models/BuyInvoices/CreateUpdateBuyInvoiceRequest.cs
@@ -4,8 +4,6 @@ namespace JewelrySalesSystem.BAL.Models.BuyInvoices
 {
     public class CreateUpdateBuyInvoiceRequest
     {
-        public int BuyInvoiceId { get; set; }
-        public string InvoiceType { get; set; } = string.Empty;
         public string CustomerName { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
         public float Total { get; set; }
