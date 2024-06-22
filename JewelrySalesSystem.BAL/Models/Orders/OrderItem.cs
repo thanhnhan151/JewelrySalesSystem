14a8d0ba4624eb28eb08c00f1bf2550689fff5f1
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-22 17:49:33 +0700

    Remove Entities

--- JewelrySalesSystem.BAL/Models/Orders/OrderItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JewelrySalesSystem.BAL.Models.Orders
-{
-    public class OrderItem
-    {
-        public string ProductName { get; set; } = string.Empty;
-        public float Total { get; set; }
-        public float PerDiscount { get; set; }
-    }
-}
