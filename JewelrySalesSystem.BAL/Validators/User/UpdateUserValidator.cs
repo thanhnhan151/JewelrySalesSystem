8011dd7c1448e3825d99c6b16919b2d54a876561
phamvietduc <phamvietduc0411@gmail.com>
2024-06-24 23:45:18 +0700

    Fix Validate Update User

--- JewelrySalesSystem.BAL/Validators/User/UpdateUserValidator.cs
+++ JewelrySalesSystem.BAL/Validators/User/UpdateUserValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using JewelrySalesSystem.BAL.Models.Users;
+using JewelrySalesSystem.DAL.Infrastructures;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,17 @@ namespace JewelrySalesSystem.BAL.Validators.User
 {
     public class UpdateUserValidator : AbstractValidator<UpdateUserRequest>
     {
-        public UpdateUserValidator()
+        private readonly IUnitOfWork _unitOfWork;
+        public UpdateUserValidator(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             RuleFor(user => user.UserName)
                 .NotEmpty()
                 .WithMessage("User name is required.")
                 .Matches("^[a-zA-Z0-9]+$")
-                .Length(2, 50);
+                .Length(2, 50)
+                .MustAsync(async (userName, cancellation) => !await CheckDuplicateAsync(userName, "userName"))
+                .WithMessage("UserName with the same name already exists.");
 
             RuleFor(user => user.FullName)
                 .NotEmpty().Length(2, 50)
@@ -26,12 +31,17 @@ namespace JewelrySalesSystem.BAL.Validators.User
             RuleFor(user => user.PhoneNumber).NotEmpty()
                 .Length(10, 10)
                 .Matches(@"\d{10}$")
-                .WithMessage("Phone number must be a valid 10-digit number.");
+                .WithMessage("Phone number must be a valid 10-digit number.")
+                .MustAsync(async (phoneNumber, cancellation) => !await CheckDuplicateAsync(phoneNumber, "phoneNumber"))
+                .WithMessage("PhoneNumber with the same name already exists.");
 
             RuleFor(user => user.Email)
                 .NotEmpty()
                 .EmailAddress()
-                .WithMessage("Email must be a valid email address."); ;
+                .WithMessage("Email must be a valid email address.")
+                .MustAsync(async (email, cancellation) => !await CheckDuplicateAsync(email, "email"))
+                .WithMessage("Email with the same name already exists.");
+
 
             RuleFor(user => user.Password)
                 .NotEmpty();
@@ -43,8 +53,21 @@ namespace JewelrySalesSystem.BAL.Validators.User
             RuleFor(user => user.RoleId)
                 .NotEmpty()
                 .GreaterThan(0)
-                .WithMessage("Role ID must be greater than 0.");
+                .WithMessage("Role ID must be greater than 0.")
+                .MustAsync(async (roleId, cancellation) => await CheckRoleId(roleId))
+                .WithMessage("Role ID does not exist .");
 
+        }
+
+        private async Task<bool> CheckDuplicateAsync(string details, string options)
+        {
+            var existing = await _unitOfWork.Users.CheckDuplicate(details, options);
+            return existing != null;
+        }
+
+        private async Task<bool> CheckRoleId(int id)
+        {
+            return await _unitOfWork.Users.CheckRoleIdExists(id);
         }
 
     }
