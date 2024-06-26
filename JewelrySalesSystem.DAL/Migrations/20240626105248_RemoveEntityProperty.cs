f9114442cef8308be90ac27e49c33fcbb4f600be
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-06-26 17:56:07 +0700

    Add Migrations

---
+++ JewelrySalesSystem.DAL/Migrations/20240626105248_RemoveEntityProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JewelrySalesSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveEntityProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaterialType",
+                table: "Product");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MaterialType",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
