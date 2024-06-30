f598ec87fbbdd8cba250e1b718cfadd10e568222
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-07-01 00:50:45 +0700

    Refactor BAL Interfaces

---
+++ JewelrySalesSystem.BAL/Interfaces/IColorService.cs
@@ -0,0 +1,11 @@
+﻿using JewelrySalesSystem.BAL.Models.Colors;
+
+namespace JewelrySalesSystem.BAL.Interfaces
+{
+    public interface IColorService
+    {
+        Task<List<GetColorResponse>> GetAllAsync();
+
+        Task<GetColorResponse?> GetByIdAsync(int id);
+    }
+}
