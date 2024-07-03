f5f1fa80bced97716302effc86ab94618a7869a4
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-07-03 16:59:20 +0700

    Refactor code

--- JewelrySalesSystem.DAL/Persistence/JewelryDbContext.cs
+++ JewelrySalesSystem.DAL/Persistence/JewelryDbContext.cs
@@ -17,15 +17,15 @@ namespace JewelrySalesSystem.DAL.Persistence
 
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("Server=(local);Uid=sa;Pwd=1;Database=JewelrySalesSystem;Trusted_Connection=true;TrustServerCertificate=true;");
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseSqlServer("Server=(local);Uid=sa;Pwd=1;Database=JewelrySalesSystem;Trusted_Connection=true;TrustServerCertificate=true;");
 
-                //optionsBuilder.UseSqlServer("Server=tcp:jewelrysalessystem.database.windows.net,1433;Initial Catalog=JewelrySalesSystem;Persist Security Info=False;User ID=jss;Password=@Testpassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
+        //        //optionsBuilder.UseSqlServer("Server=tcp:jewelrysalessystem.database.windows.net,1433;Initial Catalog=JewelrySalesSystem;Persist Security Info=False;User ID=jss;Password=@Testpassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        //    }
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
