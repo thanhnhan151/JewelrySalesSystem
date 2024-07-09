319b5270d3fba900585333339dfc650402da3ab0
thanhnhan151 <truongthanhnhangv@gmail.com>
2024-07-09 18:18:00 +0700

    Add Migrations

---
+++ JewelrySalesSystem.DAL/Migrations/20240709105436_AddInvoiceField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JewelrySalesSystem.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInvoiceField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InOrOut",
+                table: "Invoice",
+                type: "nvarchar(3)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InOrOut",
+                table: "Invoice");
+        }
+    }
+}
